7eeb176fe4a948ff839fbd42f45bd791da1eb965
TanishT <tanishtyagi@gmail.com>
2021-07-10 22:22:27 -0400

    removed octave files

--- andrew_ng/machine_learning/octave/octave_basics/graphs.m
+++
@@ -1,2 +0,0 @@
-w = -6 * sqrt(10)*rand(1, 10000)
-hist(w, 50) %creates a histogram of vector w with 50 different bars
--- andrew_ng/machine_learning/octave/octave_basics/loading_data.m
+++
@@ -1,7 +0,0 @@
-%loading in dataset
-load('ex1data1.txt')
-load('ex1data2.txt')
-
-%printing out first dataset
-ex1data1
-size(ex1data1)
